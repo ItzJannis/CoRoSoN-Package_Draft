659f538ea93a14471a67ba1d4b6d9910793c9a51
ItzJannis <itzjannis@users.noreply.github.com>
2023-09-26 23:21:48 +0200

    CoRoSoN: include other CoRoSoN modules

---
+++ CoRoSoN.h
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "CoRoSoN_Util.h"
+#include "CoRoSoN_I2C.h"
+#include "CoRoSoN_IR-Ring.h"
+#include "CoRoSoN_Pixy.h"
+#include "CoRoSoN_Compass.h"
+
+#ifndef COROSON_H
+#define COROSON_H
+
+#endif // COROSON_H
