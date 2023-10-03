35fe7b7e63c68a455d579763ea5336975cd027e3
ItzJannis <itzjannis@users.noreply.github.com>
2023-10-03 12:22:07 +0200

    ArduinoFile uses CoRoSoN_Util

--- CoRoSoN_Package.ino
+++ CoRoSoN_Package.ino
@@ -1,3 +1,18 @@
 #include "CoRoSoN.h"
-void setup() {}
-void loop() {}
+
+void setup() {
+  Serial.begin(115200);
+  //DEBUG_BLOCK("test", 1000); // uncomment this line to block the processor with periodic printing
+}
+
+void loop() {
+  ERRORS Errors = OKAY; // start with everything okay
+  DEBUG_PRINT(Errors);
+  // create bitmap to show which errors occured
+  Errors = ERROR_BREAK_OUT | PROCESS_RUNNING | INVALID_CONFIG | CONNECT_FAILED;
+  // print errors because one occured
+  DEBUG_PRINT(Errors);
+  DEBUG_ERRORS(Errors);
+  // block cpu for ever with "test" printed every 2 seconds
+  DEBUG_BLOCK("test", 2000);
+}
