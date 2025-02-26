2ea2c090af3e5f9902e0cfafe1180d229ca0f5d5
ItzJannis <itzjannis@users.noreply.github.com>
2025-02-26 22:26:53 +0100

    Added Main.ino - Only tests the IR-Ring and CoRoSoN_Main so far.

---
+++ Software/Main/Main.ino
@@ -0,0 +1,43 @@
+#include "CoRoSoN_Main.h"
+#include "../Peripheral/CoRoSoN_IR-Ring.h"
+
+IRRing irRing(I2C_ADD_IR);
+
+void setup() {
+  CoRoSoN_Init();
+}
+
+void loop() {
+  //
+  // Set LEDs based on buttons
+  //
+  if (CoRoSoN_ButtonIsPressed(I2C_MODULE_1, LEFT)) {
+    CoRoSoN_SetBoardLEDColor(LEFT, BLUE);
+    CoRoSoN_SetI2CLEDColor(I2C_MODULE_1, LEFT, BLUE);
+    CoRoSoN_SetMotor(MOTOR_1, FORWARD, 20);
+  } else {
+    CoRoSoN_SetBoardLEDColor(LEFT, OFF);
+    CoRoSoN_SetI2CLEDColor(I2C_MODULE_1, LEFT, OFF);
+    CoRoSoN_SetMotor(MOTOR_1, FORWARD, 0);
+  }
+  if (CoRoSoN_ButtonIsPressed(I2C_MODULE_1, RIGHT)) {
+    CoRoSoN_SetBoardLEDColor(RIGHT, BLUE);
+    CoRoSoN_SetI2CLEDColor(I2C_MODULE_1, RIGHT, BLUE);
+    CoRoSoN_SetMotor(MOTOR_2, FORWARD, 20);
+  } else {
+    CoRoSoN_SetBoardLEDColor(RIGHT, OFF);
+    CoRoSoN_SetI2CLEDColor(I2C_MODULE_1, RIGHT, OFF);
+    CoRoSoN_SetMotor(MOTOR_2, FORWARD, 0);
+  }
+  //
+  // Debug print ir
+  //
+  DEBUG_PRINT(irRing.SeesBall());
+  DEBUG_PRINT(irRing.BallDirection());
+  DEBUG_PRINT(irRing.BallDistance());
+  //
+  // Update
+  //
+  irRing.Update();
+  delay(5);
+}
